204ba13cc29aedb1eda42814a72fcd8a968284a6
zwh0725 <zhengwenhui0725@gmail.com>
2024-09-03 08:44:18 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'www.huihui0725.com' },
+    { title: '欢迎来到我的博客', url: '' },
   ],
 
   // 英雄区左右侧组件颠倒位置
