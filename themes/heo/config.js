ec3168f1ade2b6a96bbc830ef3a012f265c96c5d
zwh0725 <zhengwenhui0725@gmail.com>
2024-09-03 08:35:28 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,6 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -118,7 +117,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://www.bbshare.site',
+  HEO_SOCIAL_CARD_URL: '',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
